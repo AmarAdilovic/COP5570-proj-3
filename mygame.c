adaba239af7c8e032eb68ac558ca039a438f8c19
Quang Vu <quangvu@Quangs-MacBook-Pro.local>
2024-03-24 21:56:50 -0400

    add timing to game

--- mygame.c
+++ mygame.c
@@ -7,6 +7,8 @@ Purpose: contains all tic tac toe related functions
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
+
 
 /*
 Game *find_game(char *user1, char *user2): find the existing game with given username of user1 and user2 as 
@@ -25,13 +27,34 @@ Game *find_game(char *user1, char *user2) {
     return NULL;
 }
 
+/*
+int who_move(Game *ptr) this function will return whose move is next. 1 is white and 2 is black.
+*/
+int who_move(Game *ptr) {
+    int i, j, color, count;
+    count = 0;
+    // decide which color (black or white) will make the next move
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            if (ptr->board[i][j] != 0)
+                count++;
+        }
+    }
+
+    // if count is divisible by 2, next move is black
+    color = 1;
+    if (count % 2 == 0) {
+        color = 2;
+    } 
+    return color;
+}
 
 /*
 Game *create_game(char *black, char *white): create a new game with given username for black and username for white
-as argument and return the address pointer to the new game struct
+and time limit as argument and return the address pointer to the new game struct
 */
 
-Game *create_game(char *black, char *white) {
+Game *create_game(char *black, char *white, int limit) {
     // pointer to pointer of head
     Game **ptr_ptr = &game_head;
     // pointer to head
@@ -73,6 +96,10 @@ Game *create_game(char *black, char *white) {
         }
     }
 
+    time(&(ptr->last_time_black));
+    time(&(ptr->last_time_white));
+    ptr->time_left_white = limit;
+    ptr->time_left_black = limit;
     ptr->next = NULL;
     *ptr_ptr = ptr; /* This can help handle NULL(empty linked list) case */
     return ptr;
@@ -122,12 +149,13 @@ int isWin(Game *ptr) {
 int move(char *cor, Game *ptr): This function will make the move in the board given the coordinate as char and game pointer as arguments. 
 The function will decide black or white move based on the rule "black always making the first move". Function will return 0 if the move is
 success and the game is not finish, -1 if the move fail because the place is taken, -2 if the move fail because of not correct coordinate 
-format given, 1 if white win, 2 if black win, 3 if draw.
+format given, 1 if white win, 2 if black win, 3 if draw, 4 if white win because black out of time, 5 if black win because white out of time.
 */
 
 int move(char *cor, Game *ptr) {
     int i, j, color, count;
-    
+    time_t current, time_use;
+    count = 0;
     // decide which color (black or white) will make the next move
     for (i = 0; i < 3; i++) {
         for (j = 0; j < 3; j++) {
@@ -158,6 +186,22 @@ int move(char *cor, Game *ptr) {
     if (ptr->board[*cor - 'A'][*(cor+1) - '1'] != 0)
         return -1;
 
+    // set the time stamp in the game and deduct the time from time left
+    time(&current); /* get the current time */
+    if (color == 1) {
+        time_use = current - ptr->last_time_white; // calculate how many seconds passed
+        ptr->time_left_white = ptr->time_left_white - time_use; // deduct it from time left
+        ptr->last_time_black = current; // set the timer for black
+        if (ptr->time_left_white <= 0)
+            return 5; // black win because white out of time.
+    } else {
+        time_use = current - ptr->last_time_black; // calculate how many seconds passed
+        ptr->time_left_black = ptr->time_left_black - time_use; // deduct it from time left
+        ptr->last_time_white = current; // set the timer for white
+        if (ptr->time_left_black <= 0)
+            return 4; // white win because black out of time
+    }
+
     // set the position to the color
     ptr->board[*cor - 'A'][*(cor+1) - '1'] = color;
 
@@ -170,11 +214,19 @@ int move(char *cor, Game *ptr) {
 char *print_board(Game *ptr) This function will print out the board and return the string contains the new board
 */
 char *print_board(Game *ptr) {
-    int i, j, size, count;
+    int i, j, size, count, ret_val_size;
     char row;
     size = 45;
-    char *ret_val;
-    ret_val = (char*) malloc(size*sizeof(char)); /* TODO remember to free() the string after printout to client */
+    ret_val_size = 160;
+    char *ret_val, *str_ret_val;
+    
+    // Print stat
+    str_ret_val = (char*) malloc(ret_val_size*sizeof(char));
+    sprintf(str_ret_val, "Black:%15s       White:%15s\n Time:%15d        Time:%15d\n\n", ptr->black, ptr->white, (int) ptr->time_left_black, (int) ptr->time_left_white);
+    /* TODO remember to free() the string after printout to client */
+
+    // Print board
+    ret_val = (char*) malloc(size*sizeof(char)); 
     if (ret_val == NULL) {
         fprintf(stderr, "Out of memory when using print_board function\n");
         return NULL;
@@ -215,6 +267,10 @@ char *print_board(Game *ptr) {
             ret_val[count] = '\0'; /* end of string */
     }
 
-    return ret_val;
+    // concat stat and board
+    strcat(str_ret_val, ret_val);
+    free(ret_val);
+
+    return str_ret_val;
 }
 
